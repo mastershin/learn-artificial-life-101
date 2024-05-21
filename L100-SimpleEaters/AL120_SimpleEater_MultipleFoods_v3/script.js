ececac446aec09560c9e25004098702d030b5b0d
jae <jae@voxelcloud.io>
2024-05-21 14:10:34 -0700

    add github page link

--- L100-SimpleEaters/AL120_SimpleEater_MultipleFoods_v3/script.js
+++ L100-SimpleEaters/AL120_SimpleEater_MultipleFoods_v3/script.js
@@ -11,7 +11,10 @@ The eater does not have a vision.
 
 @author: Jae Shin - mastershin at gmail dot com
 @source: github.com/mastershin/learn-artificial-life-101
-@deployed: https://replit.com/@mastershin1/AL120SimpleEaterMultipleFoodsv3
+
+# Deployed at:
+- https://mastershin.github.io/learn-artificial-life-101/L100-SimpleEaters/AL120_SimpleEater_MultipleFoods_v3/
+- https://replit.com/@mastershin1/AL120SimpleEaterMultipleFoodsv3
 */
 
 // Initialize Kaboom
